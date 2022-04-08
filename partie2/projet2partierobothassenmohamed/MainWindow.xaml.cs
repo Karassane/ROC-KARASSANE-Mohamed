b362af832b907b6f38c83865da76d185b674d498
Robot <70626387+iutgeiitoulonE105@users.noreply.github.com>
2022-04-08 11:55:34 +0200

    08.04.2022
    08.04.2022

---
+++ partie2/projet2partierobothassenmohamed/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using ExtendedSerialPort;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace RobotInterface
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ReliableSerialPort serialPort1;
+        DispatcherTimer timerAffichage;
+        string receivedText = "";
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            textBoxEmission.Text = "Bonjour";
+            serialPort1 = new ReliableSerialPort("COM4", 115200, Parity.None, 8, StopBits.One);
+            serialPort1.DataReceived += SerialPort1_DataReceived;
+            serialPort1.Open();
+            
+            timerAffichage = new DispatcherTimer();
+            timerAffichage.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            timerAffichage.Tick += TimerAffichage_Tick; ;
+            timerAffichage.Start();
+        }
+
+        private void TimerAffichage_Tick(object sender, EventArgs e)
+        {
+            if(receivedText !="")
+            {
+                textBoxReception.Text += receivedText;
+                receivedText = "";
+            }
+        }
+
+        private void SerialPort1_DataReceived(object sender, DataReceivedArgs e)
+        {
+            // textBoxReception.Text 
+            receivedText += Encoding.UTF8.GetString(e.Data,0,e.Data.Length) ;
+        }
+
+        void envoyer()
+        {
+            serialPort1.Write(textBoxEmission.Text);
+            //textBoxReception.Text = textBoxReception.Text + "reçu: "+textBoxEmission.Text + "\n";
+            textBoxEmission.Clear();
+        }
+
+        private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
+        {
+            buttonEnvoyer.Background = Brushes.RoyalBlue;
+            if (buttonEnvoyer.Background == Brushes.RoyalBlue)
+            {
+                buttonEnvoyer.Background = Brushes.Beige;
+            }
+
+            else
+            {
+                buttonEnvoyer.Background = Brushes.RoyalBlue;
+            }
+
+            envoyer();
+
+        }
+
+        private void textBoxEmission_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                envoyer();
+            }
+        }
+
+        
+
+        private void buttonClear_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxReception.Clear();
+        }
+    }
+}
